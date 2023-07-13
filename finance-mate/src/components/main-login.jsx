4fc41e6e0948b696cae877182d47b4fc6906867d
Shoaib Ashfaq <shoaibashfaq111@gmail.com>
2023-07-13 08:08:26 -0400

    Adding redirection functionality to the Login Page Extracting Access token from provided by Google in the Payments component

--- finance-mate/src/components/main-login.jsx
+++ finance-mate/src/components/main-login.jsx
@@ -1,21 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { Button, TextField, Stack } from '@mui/material';
 import { BrowserRouter as Link } from 'react-router-dom';
 import GoogleButton from 'react-google-button'
+
 import Popup from 'reactjs-popup';
 import { useNavigate } from 'react-router-dom';
 import axios from "axios"
+import {gapi} from "gapi-script"
 
+
+const clientId= "686753266137-b2qsgsue7idasuae2gpbdmoba3lo9bul.apps.googleusercontent.com"
 
 function LoginMain(props) {
   const [username,setUsername]=useState();
   const [password,setPassword]=useState();
   const navigate=useNavigate();
 
+  
   const handleUsername=(event)=>{
     setUsername(event.target.value)
   }
-
   const handlePassword=(event)=>{
     setPassword(event.target.value)
   }
@@ -34,9 +38,6 @@ function LoginMain(props) {
     
   };
 
-
-
-
   const handleLocalSignUp= async(event)=>{
     // event.preventDefault()
     const signupLink="http://localhost:8080/api/signup"
@@ -52,9 +53,31 @@ function LoginMain(props) {
 
   }
 
+  
+  const OAuthRedirection=async ()=>{
+    const auth_2_url="https://accounts.google.com/o/oauth2/v2/auth"
+
+    const params = {
+      client_id: clientId,
+      redirect_uri: 'localhost:3030/payments',
+      response_type: 'token',
+      scope: 'https://www.googleapis.com/auth/drive.metadata.readonly',
+      include_granted_scopes: true,
+      state: 'pass-through value'
+    };
+
+    const queryString = Object.entries(params)
+    .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+    .join('&');
+
+    const authorizationUrl = `${auth_2_url}?${queryString}`;
+
 
   
+    window.location.href=authorizationUrl;
+  }
 
+  
   return (
     <Stack
       direction="column"
@@ -75,9 +98,9 @@ function LoginMain(props) {
         Sign UP
       </Button>
 
-     
+     <GoogleButton onClick={OAuthRedirection}></GoogleButton>
 
-    <GoogleButton/> 
+      
 
 
 
