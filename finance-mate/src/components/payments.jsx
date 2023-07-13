47bc411202658e7af11d03f56f4aba59063b0ded
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2023-07-13 10:22:42 -0400

    changes I made in the front end to test the plaid api functionality

--- finance-mate/src/components/payments.jsx
+++ finance-mate/src/components/payments.jsx
@@ -1,15 +1,15 @@
-import react from 'react'
-import { TextField, Button,Grid } from '@mui/material';
-import NavBar from './nav-bar'
-import { useLocation } from 'react-router-dom';
-import { DisabledByDefault } from '@mui/icons-material';
+import react from "react";
+import { TextField, Button, Grid } from "@mui/material";
+import NavBar from "./nav-bar";
+import { useLocation } from "react-router-dom";
+import { DisabledByDefault } from "@mui/icons-material";
 import PlaidLink from "react-plaid-link";
-
+import { Link } from "react-router-dom";
 
 const CreditCardForm = () => {
-    const location=useLocation();
-    const user=location.state
-    console.log(user)
+  const location = useLocation();
+  const user = location.state;
+  console.log(user);
   const handleSubmit = (event) => {
     event.preventDefault();
     // Add your form submission logic here
@@ -17,13 +17,13 @@ const CreditCardForm = () => {
 
   return (
     <div>
-        <NavBar></NavBar>
-        <h1>Welcome {user.username}!!!</h1>
-        
-
+      <NavBar></NavBar>
+      <h1>Welcome {user.username}!!!</h1>
+      <Link to={"/linkplaid"}>
+        <button>Connect Bank Account</button>
+      </Link>
     </div>
   );
 };
 
 export default CreditCardForm;
-
