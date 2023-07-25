41969f549c85907510d66a23b2891b1f68b8d18f
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-07-25 13:18:01 -0400

    Style add expense page

--- finance-mate/src/components/BudgetExpenseForm.jsx
+++ finance-mate/src/components/BudgetExpenseForm.jsx
@@ -18,14 +18,25 @@ import {
   MenuItem,
   Container,
   Grid,
-  Chip
+  Chip,
 } from "@mui/material";
 import styled from "styled-components";
-import waveBackground from "./layered-waves-haikei.svg";
+
 import BudgetBox from "./BudgetBox";
 import RecentExpenses from "./RecentExpenses";
 import SideBar from "./side-bar";
 const BackgroundContainer = styled.div``;
+
+const PageBackground = styled.div`
+  width: 100%;
+  height: 100vh;
+  background: linear-gradient(to right, #2d6a4f, #74c69d);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+
+`;
 
 const DottedBox = styled.div`
   border: 4px dotted #000;
@@ -38,21 +49,25 @@ const DottedBox = styled.div`
 `;
 
 const ContentContainer = styled.div`
-  position: relative;
-  z-index: 1;
+position: relative;
+z-index: 1;
+background-color: #fafafa;
+padding: 20px;
+border-radius: 15px;
+box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+margin-top: 80px; 
 `;
 
-const WaveImage = styled.img`
+const Navbar = styled.div`
   position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: auto;
+  top: 0;
+  padding: 20px 0 0 30px;
+  display: flex;
+  justify-content: flex-start;
 `;
 
-const BudgetContainer = styled.div`
-  margin-bottom: 20px;
-`;
+
+
 
 const AddExpenseForm = () => {
   const dispatch = useDispatch();
@@ -61,7 +76,7 @@ const AddExpenseForm = () => {
 
   const [expenseName, setExpenseName] = useState("");
   const [expenseAmount, setExpenseAmount] = useState("");
-  //-------------------------------//
+
   const [selectedBudgetId, setSelectedBudgetId] = useState(parseInt(budget_id));
   const [selectedBudget, setSelectedBudget] = useState(undefined);
 
@@ -70,39 +85,16 @@ const AddExpenseForm = () => {
   const expensesForBudget = all_expenses.filter(
     (expense) => expense.BudgetId === selectedBudgetId
   );
-  // console.log("expensesForBudget==>",expensesForBudget)
+
   const budget_expense_total = expensesForBudget.reduce(
     (acc, expense) => acc + parseInt(expense.expense_value),
     0
   );
-  // console.log(budget_expense_total,expensesForBudget)
-  // const budget_expense_total = useSelector((state) => state.user_budget_expenses);
-  // console.log("expenses for entertainment:", expensesForBudget)
-  // const selectedBudget = budgets.find((budget) => budget.id === selectedBudgetId);
-
-  // useEffect(() => {
-  //   dispatch(getBudgetsThunk());
-  //   dispatch(getExpensesThunk());
-  //   setSelectedBudget(budgets.find((budget) => budget.id == selectedBudgetId));
-  // }, []);
-
-  /**Each Time the user selects a budget from th edrop down menu, this methods does the following:
-   *  1.
-   */
   useEffect(() => {
     dispatch(getBudgetsThunk());
     dispatch(getExpensesThunk());
     setSelectedBudget(budgets.find((budget) => budget.id == selectedBudgetId));
-    // Add check to ensure selectedBudget is defined before dispatching
-    // if (selectedBudget) {
-    //   // console.log("budget expense thunk launched");
-    //     dispatch(getExpenseOfBudgetThunk(selectedBudget.id));
-    // }
-
-    // console.log("Use Effect");
   }, [selectedBudgetId]);
-
-  // console.log("budget_expense_total", budget_expense_total);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -114,124 +106,109 @@ const AddExpenseForm = () => {
     };
 
     dispatch(addExpenseThunk(expenseData));
-    // if (selectedBudgetId) {
-    //   dispatch(getExpenseOfBudgetThunk(selectedBudgetId));
-    // }
 
     setExpenseName("");
     setExpenseAmount("");
   };
 
   return (
-    <BackgroundContainer className="dashboard">
-      <SideBar></SideBar>
-      {/* <WaveImage src={waveBackground} alt="Wave background" /> */}
-      <Container maxWidth="sm" className="content">
-        <ContentContainer>
-          {/* <BudgetContainer>
-            <Typography variant="h6">Selected Budget:</Typography>
+    <PageBackground>
+      <Navbar>
+        <Grid container spacing={2}>
+        <Grid item>
+                <Chip
+                  label="Add Budget"
+                  component={Link}
+                  to="/budgetform"
+                  clickable
+                  sx={{
+                    backgroundColor: "limegreen",
+                    color: "#fff",
+                    "&:hover": {
+                      backgroundColor: "#05377f",
+                    },
+                  }}
+                />
+              </Grid>
+              <Grid item>
+                <Chip
+                  label="Budget Page"
+                  component={Link}
+                  to="/budget-view"
+                  clickable
+                  sx={{
+                    backgroundColor: "black",
+                    color: "white",
+                    "&:hover": {
+                      backgroundColor: "#05377f",
+                    },
+                  }}
+                />
+              </Grid>
+        </Grid>
+      </Navbar>
+          <ContentContainer>
+          <Typography
+            variant="h2"
+            align="center"
+            sx={{ color: "black", fontWeight: "bold" }}
+            gutterBottom
+          >
+            Add Expenses
+          </Typography>
+            <DottedBox>
+              <form onSubmit={handleSubmit}>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  label="Expense Name"
+                  value={expenseName}
+                  onChange={(e) => setExpenseName(e.target.value)}
+                />
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  label="Amount"
+                  type="number"
+                  value={expenseAmount}
+                  onChange={(e) => setExpenseAmount(e.target.value)}
+                />
+                <Select
+                  variant="outlined"
+                  fullWidth
+                  label="Budget Category"
+                  value={selectedBudgetId}
+                  onChange={(e) => {
+                    setSelectedBudgetId(e.target.value);
+                    // console.log("User Selected ID"+e.target.value);
+                  }}
+                >
+                  {budgets.map((budget) => (
+                    <MenuItem key={budget.id} value={budget.id}>
+                      {budget.budget_name}
+                    </MenuItem>
+                  ))}
+                </Select>
+                <Button type="submit" variant="contained" color="primary">
+                  Add Expense
+                </Button>
+              </form>
+            </DottedBox>
             {selectedBudget ? (
-              <Typography variant="body1">{selectedBudget.budget_name}</Typography>
+              <BudgetBox
+                budget={selectedBudget}
+                expenses={expensesForBudget}
+                expense_total={budget_expense_total}
+              />
             ) : (
-              <Typography variant="body1">No budget selected</Typography>
+              <p></p>
             )}
-          </BudgetContainer> */}
-          <DottedBox>
-            <form onSubmit={handleSubmit}>
-              <Typography variant="h5" gutterBottom>
-                Add Expense
-              </Typography>
-              <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                label="Expense Name"
-                value={expenseName}
-                onChange={(e) => setExpenseName(e.target.value)}
-              />
-              <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                label="Amount"
-                type="number"
-                value={expenseAmount}
-                onChange={(e) => setExpenseAmount(e.target.value)}
-              />
-              <Select
-                variant="outlined"
-                fullWidth
-                label="Budget Category"
-                value={selectedBudgetId}
-                onChange={(e) => {
-                  setSelectedBudgetId(e.target.value);
-                  // console.log("User Selected ID"+e.target.value);
-                }}
-              >
-                {budgets.map((budget) => (
-                  <MenuItem key={budget.id} value={budget.id}>
-                    {budget.budget_name}
-                  </MenuItem>
-                ))}
-              </Select>
-              <Button type="submit" variant="contained" color="primary">
-                Add Expense
-              </Button>
-            </form>
-          </DottedBox>
-          {selectedBudget ? (
-            <BudgetBox
-              budget={selectedBudget}
-              expenses={expensesForBudget}
-              expense_total={budget_expense_total}
-            />
-          ) : (
-            <p></p>
-          )}
-
-<Grid container spacing={2}>
-      <Grid item>
-        <Chip
-          label="Add Budget"
-          component={Link}
-          to="/budgetform"
-          clickable
-          sx={{
-            backgroundColor: "limegreen",
-            color: "#fff",
-            "&:hover": {
-              backgroundColor: "#05377f",
-            },
-          }}
-        />
-      </Grid>
-      <Grid item>
-        <Chip
-          label="Budget Page"
-          component={Link}
-          to="/budget-view"
-          clickable
-          sx={{
-            backgroundColor: "black",
-            color: "white",
-            "&:hover": {
-              backgroundColor: "#05377f",
-            },
-          }}
-        />
-      </Grid>
-    </Grid>
-          {/* <BudgetBox
-            budget={selectedBudget}
-            expenses={expensesForBudget}
-            expense_total={budget_expense_total}
-          /> */}
-          {/* <RecentExpenses expensesForBudget={expensesForBudget}></RecentExpenses> */}
-        </ContentContainer>
-      </Container>
-    </BackgroundContainer>
+          </ContentContainer>
+    </PageBackground>
   );
 };
 
