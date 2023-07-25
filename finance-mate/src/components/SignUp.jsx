3a3c030f790b235d70ee4643f63e819d10a89d32
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-07-24 23:05:51 -0400

    Fix erros inside side-bar.jsx and SignUp.jsx

--- finance-mate/src/components/SignUp.jsx
+++ finance-mate/src/components/SignUp.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Button, TextField } from "@mui/material";
 import axios from "axios";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { loginUserThunk } from "../redux/user/user.action";
 import {
@@ -14,8 +14,6 @@ import {
 import { BiLogIn } from "react-icons/bi";
 import GoogleButton from "react-google-button";
 import { googleLoginThunk } from "../redux/user/user.action";
-import { motion, AnimatePresence, isInView, useInView, useAnimation } from "framer-motion";
-
 
 //BiLogIn
 
@@ -107,11 +105,6 @@ const SignUp = () => {
       (prevShowPasswordVerified) => !prevShowPasswordVerified
     );
   };
-
-  const location = useLocation();
-  console.log(JSON.stringify(location))
-  console.log("Location 1: "+location)
-  
 
   return (
     <div
@@ -227,33 +220,13 @@ const SignUp = () => {
           <BiLogIn className="text-lg mr-1" />
           LOGIN
         </Link>
-      <motion.div
-      variants={{
-        hidden: {opacity: 0, x: -75},
-        visible: {opacity: 1, x: 0},
-      }}
-      initial="hidden"
-      animate="visible"
-      transition={{duration: 1, delay: 0.50}}
-      className="bg-green-600 flex rounded-lg px-4 py-2 mr-4 items-center mb-4 shadow-md border-8 ">
         <p className="text-black mr-2 font-serif text-2xl">Or sign up with:</p>
         <GoogleButton className="m-4 shadow-md hover:scale-105" onClick={googleLogin} />  
-      </motion.div>
+      </div>
 
-      <motion.div 
-      variants={{
-        hidden: {opacity: 0, x: -75},
-        visible: {opacity: 1, x: 0},
-      }}
-      initial="hidden"
-      animate="visible"
-      transition={{duration: 1, delay: 1}}
-      className="bg-green-600 flex rounded-lg px-4 py-2 mr-4 items-center mb-4 shadow-md border-8 ">
+      <div className="bg-green-600 flex rounded-lg px-4 py-2 mr-4 items-center mb-4 shadow-md border-8 ">
         <p className="text-black mr-2 font-serif text-xl">Already have an account?</p>
       </div>
-      <Link className=" hover:scale-110 hover:shadow-lg duration-200 bg-green-400 flex rounded-md px-4 py-2 mr-4 items-center mt-mb-4 shadow-md text-semibol" to="/login">
-      <BiLogIn className="text-lg mr-1"/>LOGIN</Link>
-      </motion.div>
 
       <div className="m-4 bg-green-600 flex rounded-lg px-6 py-3 mr-4 items-center mb-4 shadow-md border-8 ">
         <p className="text-base font-serif p-4 border border-green-500 border-opacity-50 rounded-lg bg-gradient-to-r from-green-100 to-green-200 text-lg">
@@ -269,27 +242,6 @@ const SignUp = () => {
           a rewarding financial journey.
         </p>
       </div>
-      <motion.div
-      variants={{
-        hidden: {opacity: 0, y: 100},
-        visible: {opacity: 1, y: 0},
-      }}
-      initial="hidden"
-      animate="visible"
-      transition={{duration: 1, delay: 1.50}}
-      className="m-4 bg-green-600 flex rounded-lg px-6 py-3 mr-4 items-center mb-4 shadow-md border-8 ">
-          <p className="text-base font-serif p-4 border border-green-500 border-opacity-50 rounded-lg bg-gradient-to-r from-green-100 to-green-200 text-lg">
-            <h1 className="flex justify-center font-semibold text-xl font-serif">Finance-Mate</h1>
-            Welcome to Finance-Mate, your trusted companion for simplified financial management. 
-            Take control of your money and achieve your goals effortlessly. 
-            Seamlessly track expenses, analyze spending patterns, and budget for a secure future. 
-            Manage accounts, make seamless transactions, and stay informed in one place. 
-            Experience convenience, security, and peace of mind. 
-            Join us today and embark on a rewarding financial journey.
-          </p>
-          
-        </motion.div>
-
     </div>
   );
 };
