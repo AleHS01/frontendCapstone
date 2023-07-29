38282f537bb03defd0677ef897238a784586d304
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-07-29 18:22:45 -0400

    Update landing page Footer 1.1

--- finance-mate/src/components/Home-Components/Footer.jsx
+++ finance-mate/src/components/Home-Components/Footer.jsx
@@ -1,67 +1,63 @@
 import React from "react";
-import { Box, Typography, Link, Grid } from "@mui/material";
-import { makeStyles } from '@mui/styles';
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    backgroundColor: '#0a210f',
-    color: '#fff',
-    padding: theme.spacing(2),
-  },
-  footerLinks: {
-    display: 'flex',
-    justifyContent: 'space-evenly',
-    listStyle: 'none',
-    padding: 0,
-    [theme.breakpoints.down('sm')]: {
-      flexDirection: 'column',
-      alignItems: 'center',
-      lineHeight: 2,
-    }
-  },
-  link: {
-    color: '#fff',
-    textDecoration: 'none',
-    '&:hover': {
-      color: '#ccc'
-    }
-  }
-}));
+import { Box, Typography, Link } from "@mui/material";
+import { Link as RouterLink } from 'react-router-dom';
 
 const Footer = () => {
-  const classes = useStyles();
-
-  const FooterLink = ({ url, name }) => (
-    <Link href={url} className={classes.link}>
-      {name}
-    </Link>
-  );
-
   return (
-    <Box className={classes.root} component="footer">
-      <Grid container alignItems="center" justifyContent="space-between">
-        <Grid item xs={12} md={4}>
-          <Typography variant="h4" component="p">
-            Made By:
-          </Typography>
-          <Typography variant="h6" component="p">
-            The boys
-          </Typography>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <ul className={classes.footerLinks}>
-            <li><FooterLink url="/about" name="About Us" /></li>
-            <li><FooterLink url="/contact" name="Contact" /></li>
-            <li><FooterLink url="/privacy-policy" name="Privacy Policy" /></li>
-            <li><FooterLink url="/terms-of-service" name="Terms of Service" /></li>
-          </ul>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <Typography variant="body2" component="p">
-            &copy; {new Date().getFullYear()} The boys
-          </Typography>
-        </Grid>
-      </Grid>
+    <Box
+      component="footer"
+      sx={{
+        backgroundColor: "#0a210f",
+        color: "#fff",
+        py: 2,
+        px: 3,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: 2
+      }}
+    >
+      <Typography variant="h4" component="div">
+        Made By:
+      </Typography>
+      <Typography variant="h6" component="div">
+        The Boys
+      </Typography>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          gap: 3,
+          width: '100%',
+          maxWidth: 500
+        }}
+      >
+        <Link 
+          to="/" 
+          component={RouterLink} 
+          sx={{ color: 'inherit', textDecoration: 'none', ':hover': { color: '#ccc' } }}
+        >
+          Home
+        </Link>
+        <Link 
+          to="/about" 
+          component={RouterLink} 
+          sx={{ color: 'inherit', textDecoration: 'none', ':hover': { color: '#ccc' } }}
+        >
+          About
+        </Link>
+        <Link 
+          to="/contact" 
+          component={RouterLink} 
+          sx={{ color: 'inherit', textDecoration: 'none', ':hover': { color: '#ccc' } }}
+        >
+          Contact
+        </Link>
+      </Box>
+      <Typography variant="body2" component="div">
+        &copy; {new Date().getFullYear()} The Boys
+      </Typography>
     </Box>
   );
 };
