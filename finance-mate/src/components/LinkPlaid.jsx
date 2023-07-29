8b51b4608f24b2845dada96b65dc5705e1193d83
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-07-29 17:15:05 -0400

    Fix plaid styling 3.5

--- finance-mate/src/components/LinkPlaid.jsx
+++ finance-mate/src/components/LinkPlaid.jsx
@@ -12,7 +12,7 @@ import { getAccountsThunk } from "../redux/accounts/account.action";
 import Accounts from "./GetAccounts";
 import SideBar from "./side-bar";
 import plaidImage1 from "../images/plaid.png"; // Import the image
-import plaidImage2 from "../images/plaid-link-img.webp"
+import plaidImage2 from "../images/plaid-link-img.webp";
 import plaidImage3 from "../images/plaid3.webp"; // Import the image
 import { ImLink } from "react-icons/im";
 
@@ -32,8 +32,6 @@ import {
 import { motion, useInView, useAnimation } from "framer-motion";
 import LinkPlaidBanner from "./LinkPlaidBanner";
 import PageHeader from "./PageHeader";
-
-
 
 //ImLink
 //import plaidImage4 from "../images/creditcards.p"; // Import the image
@@ -93,8 +91,6 @@ const LinkPlaid = () => {
     open();
   };
 
-  
-
   return (
     <div className="dashboard">
       <SideBar />
@@ -121,12 +117,9 @@ const LinkPlaid = () => {
               onClick={popUp}
               disabled={!ready}
             >
-             Link with Plaid
+              Link with Plaid
               <ImLink className="ml-2" />
             </motion.button>
-
-
-
 
             {/* <motion.img
               variants={{
@@ -157,7 +150,7 @@ const LinkPlaid = () => {
               <ImLink className="ml-2" />
             </motion.button> */}
             <br />
-            
+
             {/* <motion.h1
               variants={{
                 hidden: { opacity: 0, x: -80 },
@@ -181,33 +174,55 @@ const LinkPlaid = () => {
               //className="flex justify-evenly border border-black border-opacity-50 rounded-lg bg-gradient-to-r from-green-200 to-green-400"
             >
               <div style={{ display: "flex", justifyContent: "center" }}>
-              <motion.h1
-              variants={{
-                hidden: { opacity: 0, x: -80 },
-                visible: { opacity: 1, x: 0 },
-              }}
-              initial="hidden"
-              animate="visible"
-              transition={{ duration: 0.7, delay: 0.5 }}
-              className="font-bold font-serif text-xl "
-            >
-              Experience Secure Banking, Powered by Plaid
-            </motion.h1>
-              <motion.img
-                variants={{
-                  hidden: { opacity: 0, x: -80 },
-                  visible: { opacity: 1, x: 0 },
-                }}
-                initial="hidden"
-                animate="visible"
-                transition={{ duration: 0.8, delay: 0.65 }}
-                className="ml-4"
-                src = "https://i.postimg.cc/j2SWn1x6/plaid-link-img.webp"
-                alt="Plaid"
-                style={{ width: "700px", height: "auto" }}
-              />
+                <motion.h1
+                  variants={{
+                    hidden: { opacity: 0, x: -80 },
+                    visible: { opacity: 1, x: 0 },
+                  }}
+                  initial="hidden"
+                  animate="visible"
+                  transition={{ duration: 0.7, delay: 0.5 }}
+                  className="font-bold font-serif text-xl "
+                >
+                  Experience Secure Banking, Powered by Plaid
+                </motion.h1>
+                <motion.p
+                  variants={{
+                    hidden: { opacity: 0, x: -80 },
+                    visible: { opacity: 1, x: 0 },
+                  }}
+                  initial="hidden"
+                  animate="visible"
+                  transition={{ duration: 0.7, delay: 0.5 }}
+                  className="font-serif text-md "
+                >
+                  Plaid is a trusted financial services company that uses
+                  technology to connect with banks and manage your financial
+                  data. By integrating with Plaid, we provide you with a
+                  seamless, secure banking experience. With Plaid, your
+                  transactions and financial data remain secure and private. In
+                  fact, Plaid uses the same encryption and security measures as
+                  other world-leading financial institutions. So, you can rest
+                  assured that your banking is safe with us.
+                </motion.p>
+                This is just an example. You can change the content to match the
+                exact details and benefits of the Plaid service in relation to
+                your application.
+                <motion.img
+                  variants={{
+                    hidden: { opacity: 0, x: -80 },
+                    visible: { opacity: 1, x: 0 },
+                  }}
+                  initial="hidden"
+                  animate="visible"
+                  transition={{ duration: 0.8, delay: 0.65 }}
+                  className="ml-4"
+                  src="https://i.postimg.cc/j2SWn1x6/plaid-link-img.webp"
+                  alt="Plaid"
+                  style={{ width: "700px", height: "auto" }}
+                />
               </div>
-             
+
               {/* <motion.img
                 variants={{
                   hidden: { opacity: 0, x: -80 },
@@ -227,9 +242,7 @@ const LinkPlaid = () => {
         </Grid>
       </div>
     </div>
-
   );
 };
 
 export default LinkPlaid;
-
