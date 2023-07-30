577ca5b589d8b83e5fe2bba8d833a6c23d67fd56
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-07-29 21:59:27 -0400

    Fix SignUp page

--- finance-mate/src/components/SignUp.jsx
+++ finance-mate/src/components/SignUp.jsx
@@ -179,35 +179,14 @@ const SignUp = () => {
         className="mb-4 py-2 px-4 border border-black rounded-md shadow-lg focus:outline-none focus:border-green-500"
         style={{width: "300px"}}
       />
-      
-      <TextField
+
+      <input
         type={showPasswordVerified ? "text" : "password"}
         placeholder="Re-enter Password"
         value={passwordVerified}
         onChange={(e) => setPasswordVerified(e.target.value)}
-        style={{
-          marginBottom: "10px",
-          height:"5%",
-          width: "12.5%", // Adjust width as needed
-          // Add other styles as needed
-        }}
-        InputProps={{
-          endAdornment: (
-            <>
-              {showPasswordVerified ? (
-                <AiOutlineEyeInvisible
-                  className="password-toggle"
-                  onClick={handleTogglePasswordVerified}
-                />
-              ) : (
-                <AiOutlineEye
-                  className="password-toggle"
-                  onClick={handleTogglePasswordVerified}
-                />
-              )}
-            </>
-          ),
-        }}
+        className="mb-4 py-2 px-4 border border-black rounded-md shadow-lg focus:outline-none focus:border-green-500"
+        style={{width: "300px"}}
       />
 
       {formError && <p style={{ color: "red" }}>{formError}</p>}
@@ -219,68 +198,31 @@ const SignUp = () => {
         <AiOutlineFileDone className="text-3xl" />
         Sign Up
       </button>
+      <GoogleButton className="m-4 shadow-md hover:scale-105" onClick={googleLogin} />
+
+      <motion.div
+      variants={{
+        hidden: { opacity: 0, x: -75 },
+        visible: { opacity: 1, x: 0 },
+      }}
+      initial="hidden"
+      animate="visible"
+      transition={{ duration: 1, delay: 1 }}
+      className="bg-green-200 flex rounded-lg px-4 py-2 items-center shadow-md border-2"
+    >
+      <p className="text-black mr-2 font-serif text-lg">Already have an account?</p>
+      <Link
+        className="hover:scale-110 hover:shadow-lg duration-200 bg-green-400 flex rounded-md px-2 py-2 mr-4 items-center mt-mb-4 shadow-md text-semibold"
+        to="/login"
+      >
+        <BiLogIn className="text-lg mr-2 text" />
+        LOGIN
+      </Link>
+    </motion.div>
+      </div>
       <br />
-
-
-
-      <motion.div
-      variants={{
-        hidden: {opacity: 0, x: -75},
-        visible: {opacity: 1, x: 0},
-      }}
-      initial="hidden"
-      animate="visible"
-      transition={{duration: 1, delay: 0.50}}
-      className="bg-green-600 flex rounded-lg px-4 py-2 mr-4 items-center mb-4 shadow-md border-8 ">
-        <p className="text-black mr-2 font-serif text-2xl">Or sign up with:</p>
-        <GoogleButton className="m-4 shadow-md hover:scale-105" onClick={googleLogin} />  
-      </motion.div>
-
-      <motion.div
-      variants={{
-        hidden: {opacity: 0, x: -75},
-        visible: {opacity: 1, x: 0},
-      }}
-      initial="hidden"
-      animate="visible"
-      transition={{duration: 1, delay: 1}}
-      className="bg-green-600 flex rounded-lg px-4 py-2 mr-4 items-center mb-4 shadow-md border-8 ">
-        <p className="text-black mr-2 font-serif text-xl">Already have an account?</p>
-        <Link
-          className=" hover:scale-110 hover:shadow-lg duration-200 bg-green-400 flex rounded-md px-4 py-2 mr-4 items-center mt-mb-4 shadow-md text-semibol"
-          to="/login"
-        >
-          <BiLogIn className="text-lg mr-1" />
-          LOGIN
-        </Link>
-      </motion.div>
-
-      <motion.div
-      variants={{
-        hidden: {opacity: 0, y:100},
-        visible: {opacity: 1, y: 0},
-      }}
-      initial="hidden"
-      animate="visible"
-      transition={{duration: 1, delay: 1.50}}
-      className="m-4 bg-green-600 flex rounded-lg px-6 py-3 mr-4 items-center mb-4 shadow-md border-8 ">
-        <p className="text-base font-serif p-4 border border-green-500 border-opacity-50 rounded-lg bg-gradient-to-r from-green-100 to-green-200 text-lg">
-          <h1 className="flex justify-center font-semibold text-xl font-serif">
-            Finance-Mate
-          </h1>
-          Welcome to Finance-Mate, your trusted companion for simplified
-          financial management. Take control of your money and achieve your
-          goals effortlessly. Seamlessly track expenses, analyze spending
-          patterns, and budget for a secure future. Manage accounts, make
-          seamless transactions, and stay informed in one place. Experience
-          convenience, security, and peace of mind. Join us today and embark on
-          a rewarding financial journey.
-        </p>
-      </motion.div>
     </div>
     </div>
-    
-   
    
   );
 };
